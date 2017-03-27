545037fc326fffc59a4792c40b4e82a61a730276
Javar Alexander <jaalexander@csumb.edu>
2017-03-27 03:27:14 +0000

    Added spotify container, and feature to the main page for now, will move to the play page once code is cleaned up

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os, random
 import flask 
-import flask_socketio
-
+import flask_socketio, requests
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
 
@@ -51,6 +50,30 @@ def createGrid(size):
 def generateKey():
     key = os.urandom(24).encode('hex')
     return key
+
+# Function that Javar wrote. Fetches data from the Spotify API and display it
+@socketio.on('Spotify')
+def spotify(data):
+	tracks =[]
+	searchType = data['searchType']
+	searchQuery =  data['searchQuery']
+	searchQuery1 = searchQuery.replace("+", "%20")
+	response = requests.get("https://api.spotify.com/v1/search?q="+searchQuery1+"&type="+searchType)
+	json = response.json()
+	if 'tracks' in json and 'items' in json['tracks']:
+		for item in json['tracks']['items']:
+			print item['uri']
+    		tracks.append(item['uri'])
+
+	my_headers = {"Accept" : "application/json", "Authorization" : "Bearer BQCm9bzjiDxNb9FurI8AWVgraOhvdZyzpBBNq753DwEXocrLa8kyPNOalfXuevtiZ10Kt8FIuvM1RMnv6mWiVsz9bXU8VQzEv3xdHAE5Qs4-eFI4dh3spBArHnzQLl6gGqvddte-H7JZQVzJEsxobx1TSStfVqonFzxWdH418b5RtzgZMHFgnKtV-6qW9g_axQ1bKwQ4Fm8e1NI"}
+	url = "https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY"
+	track_response = requests.get(url, headers= my_headers)
+	spotify_links = track_response.json()
+	random_track = random.choice(tracks)
+
+	random_track_link = "https://embed.spotify.com/?uri="+random_track
+	socketio.emit('fromSpotify', random_track_link)
+
 
 socketio.run(
     app,
--- scripts/Content.js
+++ scripts/Content.js
@@ -1,7 +1,66 @@
 import * as React from 'react';
-
+//import Sound from './Sound'
+import { Socket } from './Socket';
 export class Content extends React.Component {
     render() {
-        return <div><a href = '/play'>Play Game</a></div>;
+        return (
+        <div>
+        <div><a href = '/play'>Play Game</a></div>
+       <div className = "spotifyContainer">
+              <Sound/> 
+        </div>
+        </div>
+      
+    )}
+}
+export class Sound extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            track : " "
+        };
+    }
+    //here were checking to see if someone sent spotify data and if so, let's capture that data
+    componentDidMount() {
+    Socket.on('fromSpotify' ,(data) =>{
+        this.setState({
+            'track' : data 
+            });
+            //let's make the spotify widget visible 
+        document.getElementById("Spotifyframe").style.visibility ="visible";
+    });
+    }
+    //this is what handles our spotify search button queries. Once the user hits submit that data is sent through a socket to the server and processed server side. 
+     handleSubmitMusic(event) {
+         event.preventDefault();
+         var searchType = document.getElementById('SearchFor').value;
+         var searchQuery = document.getElementById('searchQuery').value;
+        Socket.emit('Spotify' , {
+            'searchType' : searchType, 
+            'searchQuery' : searchQuery,
+        });
+     }
+     //here is where we piece everything together and render the spotify button on the screen, search box and button. 
+      render() {
+        return (
+        <div>    
+        <div>
+        <br />
+        <br />
+         <iframe id ="Spotifyframe" src={this.state.track}  frameborder="0" allowtransparency="true"></iframe>
+         </div>
+         <div className = "spotifyinput">
+            <form onSubmit={this.handleSubmitMusic}>
+                <select id = "SearchFor">
+                <option value="Track" >Track</option>
+                </select>
+                <input type = "text" id = "searchQuery" name="searchQuery"/>
+                <input type="submit" id = "submit" value = "search song!" />
+            </form>
+            </div>
+            <div>
+            </div>
+            </div>
+        );
     }
 }

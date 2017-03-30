2682e0959b0a10fb901a2191be5bb739b7956aee
Dante LaceyThompson <dlacey-thompson@csumb.edu>
2017-03-30 20:28:49 +0000

    FB login react

---
+++ scripts/Login.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { FacebookLogin } from 'react-facebook-login-component';
+
+export class Login extends React.Component{
+    constructor(props, context){
+        super(props, context);
+    }
+    responseFacebook(response){
+        console.log(response);
+        //Code implementation here
+        console.log(response.accessToken);
+        //window.location.href = "https://www.facebook.com"; checking response
+        if(response.accessToken.length > 0){
+            window.location.href = "https://www.facebook.com";
+        }
+        else{
+            window.location.reload();
+        }
+    }
+    
+    render(){
+        return (
+            <div id="fbl" className="fbl">
+                <FacebookLogin socialId="1566496380057860"
+                language="en_US"
+                scope="public_profile, email"
+                responseHandler={this.responseFacebook}
+                xfbml={true}
+                version="v2.8"
+                class="facebook-login"
+                buttonText="Login With Facebook"/>
+            </div>
+        );
+    }
+}

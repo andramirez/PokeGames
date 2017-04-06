bcf2a36d566657047dc9a50bbdbddb95faf90750
andramirez <andramirez@csumb.edu>
2017-04-06 21:30:32 +0000

    Game Set-Up form

--- scripts/Form.js
+++ scripts/Form.js
@@ -36,6 +36,31 @@ export class Form extends React.Component {
                     <input type="text" name="key" onChange={this.handleChange}></input>
                     <button>Play</button>
                 </fieldset>
+                <fieldset>
+                    <h1>PokeGames Set-Up</h1>
+                    <div class="settings-Container">
+                        <div class="settings-GameName">
+                            <legend>Game Name:</legend> 
+                            <input type="textarea" name="name"/>
+                        </div>
+                        <div class="settings-Players">
+                            <form>
+                                Number of Players: 
+                                <input type="number"  min="2" max="36" step="1" value="10"/>
+                            </form>
+                        </div>
+                        <div class="settings-hp">
+                            <form>
+                                Start HP: 
+                                <input type="number"  min="10" max="100" step="10" value="50"/>
+                            </form>
+                        </div>
+                        <div class="settings-Song">
+                            Starting Song: 
+                            <input type="textarea" name="song"/>
+                        </div>
+                    </div>
+                </fieldset>
             </form>
         );
     }

0067b8878fd12752e518c4d9e45b7abe5dcc7d8b
andramirez <andramirez@csumb.edu>
2017-04-11 20:09:53 +0000

    Test fix AGAIN

--- tests.py
+++ tests.py
@@ -7,8 +7,8 @@ import pokeAPI
 class FunctionResponseTest(unittest.TestCase):
     #1.Tests chat bot where input is not a command
     def test_getTypeByName(self):
-        response = pokeAPI.getStatsByName('bulbasaur')
-        self.assertEquals(response, "[u'grass', u'poison']")
+        response = pokeAPI.getTypeByName('bulbasaur')
+        self.assertEquals(response, [u'grass', u'poison'])
         
         
 if __name__=='__main__':
